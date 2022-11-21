68af9a67147b4d6031f9c2759af4d56203525b77
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-21 15:45:08 -0700

    Rearrange

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,7 @@
 import express from "express";
-const router = express.Router();
-
-
 import { createUser, getAllUsers, getUser, removeUser, updateUser } from '../controllers/users.js';
+
+const router = express.Router();
 
 // All the routes will start with '/users' (as seen in the index.js file), so we just need to route to '/'
 // Get all users
