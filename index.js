66b117bc0a31a1e87f355ad40986dc3732ff9bee
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-21 13:53:41 -0700

    Create a get request and send a message to test it

--- index.js
+++ index.js
@@ -8,5 +8,10 @@ const PORT = 8080;
 // Initialize bodyParser middleware
 app.use(bodyParser.json());
 
+// Get request to the home page ('/')
+app.get('/', (req, res) => {
+  res.send("Hello from HOME")
+})
+
 // Listen to requests on our port which runs a callback function
 app.listen(PORT, () => console.log(`Server is running on: http://localhost:${PORT}`));
