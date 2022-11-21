a259a397b8a20b8465dda24d0bc0df5f0f6ccc82
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-21 14:00:55 -0700

    Import and use userRoutes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
+// import the routes for users
+import userRoutes from './routes/users.js';
+
 // our entire application lays within this app variable
 const app = express();
 const PORT = 8080;
 
 // Initialize bodyParser middleware
 app.use(bodyParser.json());
+
+app.use('/users', userRoutes)
 
 // Get request to the home page ('/')
 app.get('/', (req, res) => {
