fc4f1ce39b32d1151aff9e2f5828f4e3edc52154
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-21 13:39:44 -0700

    Create our server to listen on port 8080

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
+// our entire application lays within this app variable
+const app = express();
+const PORT = 8080;
+
+// Initialize bodyParser middleware
+app.use(bodyParser.json());
+
+// Listen to requests on our port which runs a callback function
+app.listen(PORT, () => console.log(`Server is running on: http://localhost:${PORT}`));
