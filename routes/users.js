cb7213fade1a3360d24f5fffae61c8d8cb43f6b1
Jeandre Visser <jeandrev1414@gmail.com>
2022-11-21 14:13:11 -0700

    Test out request for users by sending a users array

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 import express from "express";
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "Dwayne",
+    lastName: "Johnson",
+    age: 24
+  },
+  {
+    firstName: "Philip",
+    lastName: "Philips",
+    age: 34
+  },
+]
+
 // All the routes will start with '/users' (as seen in the index.js file), so we just need to route to '/'
 router.get('/', (req, res) => {
-  res.send("HELLO from first route")
+  console.log(users)
+  res.send(users)
 })
 
 export default router;
